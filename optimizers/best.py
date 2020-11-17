7d87a4e560e2884cb37060554c7fdc4a178feca4
Rayane AMROUCHE <rayane.amrouche@epita.fr>
2020-11-17 15:32:50 +0100

    naming update

--- optimizers/best.py
+++ optimizers/best.py
@@ -7,35 +7,34 @@ import numpy as np
 import pandas as pd
 
 def best_sharper(type, nb):
-    arr = choose_from(start_period, end_period, nb // 2, True)
-    arr2 = choose_from(start_period, end_period, nb // 2, False)
+    stock_ids = choose_from(start_period, end_period, nb // 2, True)
+    fund_ids = choose_from(start_period, end_period, nb // 2, False)
     portefolio_id = get_epita_portfolio_id()
     portefolio = get_epita_portfolio()
+
     print("STOCKS")
     if(type == "pso"):
-        stock_part = pso_portfolio(arr)
+        stock_part = pso_portfolio(stock_ids)
     elif(type == "neo"):
-        stock_part = neo_opti_portfolio(arr)
+        stock_part = neo_opti_portfolio(stock_ids)
     else:
-        stock_part = pso_portfolio(arr)
+        stock_part = pso_portfolio(stock_ids)
     
     print("NOT STOCKS")
     if(type == "pso"):
-        fund_part = pso_portfolio(arr2)
+        fund_part = pso_portfolio(fund_ids)
     elif(type == "neo"):
-        fund_part = neo_opti_portfolio(arr2)
+        fund_part = neo_opti_portfolio(fund_ids)
     else:
-        fund_part = pso_portfolio(arr2)
+        fund_part = pso_portfolio(fund_ids)
 
-    print("GATHER")
+    print("GATHER ALL")
     stock_part = stock_part * 0.51
     fund_part = fund_part * 0.49
     final_part = np.concatenate((stock_part, fund_part))
-    asset_ids = np.concatenate((arr, arr2))
+    asset_ids = np.concatenate((arr, fund_ids))
+
     assets_dataframe = pd.DataFrame(data={'asset_id': asset_ids, 'quantities': final_part})
-    # Put portfolio
-    print(assets_dataframe)
     put_portfolio(portefolio_id, portefolio, assets_dataframe)
     post_operations([12], [portefolio_id], start_period, end_period).values[0, 0]
     print("sharp of portfolio =", post_operations([12], [portefolio_id], start_period, end_period).values[0, 0])
-    print(get_epita_portfolio())
