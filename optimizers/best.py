e9dce8a2fdd4c5ac4d86a7029db0f7e1766e60bb
Rayane AMROUCHE <rayane.amrouche@epita.fr>
2020-11-20 18:44:22 +0100

    stocks only and new constraints

--- optimizers/best.py
+++ optimizers/best.py
@@ -1,5 +1,5 @@
 from DolphinApi.config import *
-from optimizers.weights import pso_optimise, scipy_optimise
+from optimizers.weights import pso_optimise, scipy_optimise, stock_constraint, nav_constraint
 
 from optimizers.portfolio import *
 from optimizers.utils import *
@@ -44,10 +44,11 @@ def post_operations(ratios, ids, start, end, bench=None, frequency=None):
 
 
 def check_constraints(assets_ids, x):
-    print("stock part %:", stock_constraint(x, assets_ids)*100)
-    print("%nav between 0.01 and 0.1:", np.all(x <= 0.1) and np.all(x >= 0.01))
-    print("assets between 15 and 40:", len(
-        assets_ids) > 14 and len(assets_ids) < 41)
+    prices = get_prices(assets_ids)
+    stocks = get_types_ids(assets_ids, ["STOCK"])
+    print("stock part %:", stock_constraint_b(x, prices, np.array(stocks).astype(int))*100)
+    print("%nav between 0.01 and 0.1:", stock_constraint_b(x, prices, np.array(stocks).astype(int)))
+    print("assets between 15 and 40:", len(assets_ids) > 14 and len(assets_ids) < 41)
 
 
 def sharping_together(algo_opti):
@@ -93,10 +94,40 @@ def sharping_together(algo_opti):
     return final_part
 
 
+def sharping_stocks(algo_opti):
+    stock_ids = select_type(["STOCK"])
+    portefolio_id = get_epita_portfolio_id()
+    portefolio = get_epita_portfolio()
+
+    print("STOCKS")
+    stock_part = algo_opti(stock_ids, True)
+    df = pd.DataFrame(np.stack((stock_ids, stock_part), axis=-1),
+                      columns=["ids", "part"]).sort_values(by="part").values
+    final_ids = df[:, 0][::-1][:40].astype(int)
+
+
+    print("COMPUTE BEST")
+    final_part = algo_opti(final_ids, False)
+
+    check_constraints(final_ids, final_part)
+
+    assets_dataframe = pd.DataFrame(
+        data={'asset_id': final_ids, 'quantities': final_part * 1000000})
+
+    print(assets_dataframe)
+    put_portfolio(portefolio_id, portefolio, assets_dataframe)
+    post_operations([12], [portefolio_id], start_period,
+                    end_period).values[0, 0]
+    print("sharp of portfolio =", post_operations(
+        [12], [portefolio_id], start_period, end_period).values[0, 0])
+
+    return final_part
+
+
 def get_best_weigth(algo):
     if(algo == "scipy"):
-        return sharping_together(scipy_optimise)
+        return sharping_stocks(scipy_optimise)
     elif (algo == "pso"):
-        return sharping_together(pso_optimise)
+        return sharping_stocks(pso_optimise)
     else:
         print("choose an algorithm : 'pso' or 'scipy'")
